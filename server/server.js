d936339d91de42d82160b942439435556faffa92
Teja <venkatasurya.tejamatcha@freshworks.com>
2019-11-29 01:30:00 +0530

    Add entity with name

--- server/server.js
+++ server/server.js
@@ -6,9 +6,11 @@ exports = {
   ],
 
   onExternalEventHandler: function (payload) {
+    console.log('onExternalEventHandler');
+    
     let payloadData = payload.data || payload.body;
     payloadData = typeof payloadData === 'string' ? JSON.parse(payloadData) : payloadData;
-    console.log({payloadData})
+    console.log({payloadData, 'payload.requestEvent': payload.requestEvent})
 
     if(payload.requestEvent === 'createContact'){
       $request.post('https://ashishtestfs2.freshsales.io/contacts.json',
@@ -18,7 +20,23 @@ exports = {
         method: "POST"
       }).then((response) => {
         console.info('Success', response);
-        renderData();
+        renderData(response);
+      }, error => {
+        console.error('Error');
+        console.error(error)
+        renderData({ message: error.message });;
+      })  
+    }
+
+    if(payload.requestEvent === 'createLead'){
+      $request.post('https://ashishtestfs2.freshsales.io/leads.json',
+      {
+        headers: payload.headers,
+        json: payloadData,
+        method: "POST"
+      }).then((response) => {
+        console.info('Success', response);
+        renderData(response);
       }, error => {
         console.error('Error');
         console.error(error)
